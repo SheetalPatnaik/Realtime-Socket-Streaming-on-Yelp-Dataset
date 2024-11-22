e4b2913cd256cf8342fab0fd34063c777b244f35
Sheetal Patnaik <64140457+SheetalPatnaik@users.noreply.github.com>
2024-11-21 18:00:01 -0800

    Kafka cluster and Schema Registry
    Kafka cluster and Schema Registry

---
+++ src/config/config.py
@@ -0,0 +1,19 @@
+config = {
+    "openai": {
+        "api_key": "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"
+    },
+    "kafka": {
+        "sasl.username": "AFU7PJFF47L6TLPI",
+        "sasl.password": "xESJBQjwTE43BOi/R17zNobCF+KlpL7/r11ivynFynwIw6grqkErdXOS7gCKmFo4",
+        "bootstrap.servers": "pkc-n3603.us-central1.gcp.confluent.cloud:9092",
+        'security.protocol': 'SASL_SSL',
+        'sasl.mechanisms': 'PLAIN',
+        'session.timeout.ms': 50000,
+
+    },
+    "schema_registry": {
+        "url": "https://psrc-wrp99.us-central1.gcp.confluent.cloud",
+        "basic.auth.user.info": "47S26BFUZN7MFNXD:H0wa62IOomnX6huz29v7TJJEcTS6JV4Bm9r9Ywelhtq+FzOJFB3Gbe80cDf0PrOF"
+
+    }
+}
